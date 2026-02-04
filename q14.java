9c675a1a522945317b72949a77c1e372ed98e05c
santhoshr2204 <santhoshr2204@gmail.com>
2026-02-04 15:14:54 +0530

    Add files via upload
    need changes in digit optimization to avoid shape distortion.

---
+++ q14.java
@@ -0,0 +1,31 @@
+package lab_2;
+
+import java.util.Scanner;
+
+public class q14{
+	public static void main(String[] args) {
+		Scanner scan =new Scanner(System.in);
+		System.out.print("Enter value: ");
+		int x =scan.nextInt();
+		int count=1;
+		for(int j=1;j<=x;j++) {
+			for(int i =x-j;i>0;i--) {
+				System.out.print(" ");
+			}
+			for(int q=1;q<=(2*j-1);q++) {
+				System.out.print(" ");
+				System.out.print(count);
+				count+=1;
+				System.out.print(" ");
+			}
+			for (int w=(x-j);w>0;w--) {
+				if(w==1) {
+					System.out.println(" ");
+				}
+				else {
+					System.out.print(" ");
+				}
+			}
+		}
+	}
+}
